a00ef627a70654e78a18e20074d0c29e8d4efadc
GJYNBB <GJYNBB@users.noreply.github.com>
2026-05-19 20:24:20 +0800

    feat(B0-B.5): Vue Router with role-based guards and placeholder pages

---
+++ acrqg-web/src/pages/notfound/NotFoundPage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goHome() {
+  router.push('/dashboard')
+}
+</script>
+
+<template>
+  <div class="notfound-page">
+    <el-result icon="info" title="404" sub-title="抱歉，您访问的页面不存在">
+      <template #extra>
+        <el-button type="primary" @click="goHome">返回工作台</el-button>
+      </template>
+    </el-result>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.notfound-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 60vh;
+}
+</style>
